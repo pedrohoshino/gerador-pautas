49b339eda75d6f73fbdbee021423f1481f1562ec
Pedro <pedro.hoshino@sensedia.com>
2016-11-16 00:34:45 -0200

    mundando projeto

---
+++ src/main/java/br/unicamp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.unicamp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Iniciando Gerador";
+	}
+}
